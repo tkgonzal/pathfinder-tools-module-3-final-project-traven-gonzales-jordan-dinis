f8ebfe171b69288826747bf3f4b0e3bef4666456
Jordan Dinis <jtyler9999@gmail.com>
2023-07-19 19:45:57 -0700

    (feat): Landing page basics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import { Outlet, Link } from "react-router-dom"
+import "./Header.css"
 
 function Header() {
     return (
-        <header>
-            <h1>pathfinder tools</h1>
+        <header className= "d-flex flex-column align-items-center logo">
+            <h1 className="logo--txt">Pathfinder Tools</h1>
             <nav>
-                <ul>
+                <ul className = "d-flex flex-row justify-content-center gap-4 nav--links">
                     <li>
                         <Link to={`/`}>Home</Link>
                     </li>
                     <li>
-                        <Link to={`encounter-calculator`}>Encounter Calculator</Link>
+                        <Link to={`/encounter-calculator`}>Encounter Calculator</Link>
                     </li>
                 </ul>
             </nav>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,33 @@
 import Header from "../components/Header"
+import heroImg from "../assets/hero.png"
+import infoImg from "../assets/info_blurb.jpg"
+
+import "./LandingPage.css"
 
 function LandingPage() {
     return (
         <div>
             <Header />
-            <>this is a landing page</>
+            <div className="Container w-100 d-flex flex-column align-items-center">
+                <div className="d-flex flex-row justify-content-evenly landing--rows">
+                    <img className = "hero--img" src = {heroImg}></img>
+                    <div>
+                        Hero
+                    </div>
+                </div>
+                <div className="d-flex flex-row justify-content-evenly landing--rows">
+                    <div>
+                        Info Blurb
+                    </div>
+                    <img className = "info--blurb--img" src = {infoImg}></img>
+                </div>
+                <div className="w-100 d-flex flex-row justify-content-evenly landing--rows">
+                    <div className="call--to--action">
+
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
