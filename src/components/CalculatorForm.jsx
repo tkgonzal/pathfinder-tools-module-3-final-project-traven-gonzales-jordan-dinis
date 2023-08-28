20fb67fb112507b543aea92c9c77a9727a099670
Traven Gonzales <travengonzales@gmail.com>
2023-08-28 16:01:11 -0700

    feat(encounter-page-responsivity): Added mobile responsivity to EncounterPage

--- src/components/CalculatorForm.jsx
+++ src/components/CalculatorForm.jsx
@@ -13,49 +13,51 @@ function CalculatorForm(props) {
                 <hr />
             </div>
             <div className="calc-form--content">
-                <span>
-                    <label htmlFor="partyLevel">Party Level: </label>
-                    <input
-                        name="partyLevel"
-                        type="number"
-                        min="1"
-                        max="20"
-                        value={props.calcInput.partyLevel}
-                        onKeyPress={(event) => props.preventKeyPress(event)}
-                        onChange={(event) => props.onChange(event)}
-                        required
-                    />
-                </span>
+                <div className="calc-form--inputs">
+                    <span>
+                        <label htmlFor="partyLevel">Party Level: </label>
+                        <input
+                            name="partyLevel"
+                            type="number"
+                            min="1"
+                            max="20"
+                            value={props.calcInput.partyLevel}
+                            onKeyPress={(event) => props.preventKeyPress(event)}
+                            onChange={(event) => props.onChange(event)}
+                            required
+                        />
+                    </span>
 
-                <span>
-                    <label htmlFor="partySize">Party Size:</label>
-                    <input
-                        name="partySize"
-                        type="number"
-                        min="2"
-                        max="10"
-                        value={props.calcInput.partySize}
-                        onKeyPress={(event) => props.preventKeyPress(event)}
-                        onChange={(event) => props.onChange(event)}
-                        required
-                    />
-                </span>
+                    <span>
+                        <label htmlFor="partySize">Party Size:</label>
+                        <input
+                            name="partySize"
+                            type="number"
+                            min="2"
+                            max="10"
+                            value={props.calcInput.partySize}
+                            onKeyPress={(event) => props.preventKeyPress(event)}
+                            onChange={(event) => props.onChange(event)}
+                            required
+                        />
+                    </span>
 
-                <span>
-                    <label htmlFor="threatLevel">Threat Level:</label>
-                    <select
-                        name="threat"
-                        value={props.calcInput.threat}
-                        onChange={(event) => props.onChange(event)}
-                        required
-                    >
-                        <option value="trivial">Trivial</option>
-                        <option value="low">Low</option>
-                        <option value="moderate">Moderate</option>
-                        <option value="severe">Severe</option>
-                        <option value="extreme">Extreme</option>
-                    </select>
-                </span>
+                    <span>
+                        <label htmlFor="threatLevel">Threat Level:</label>
+                        <select
+                            name="threat"
+                            value={props.calcInput.threat}
+                            onChange={(event) => props.onChange(event)}
+                            required
+                        >
+                            <option value="trivial">Trivial</option>
+                            <option value="low">Low</option>
+                            <option value="moderate">Moderate</option>
+                            <option value="severe">Severe</option>
+                            <option value="extreme">Extreme</option>
+                        </select>
+                    </span>
+                </div>
 
                 <button
                     type="submit"
